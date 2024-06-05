5b7c723fc8eeb911f00b22bbb7c10ff66d00ca2c
hayliepedersen <pedersenhaylie6@gmail.com>
2024-06-05 16:29:36 -0400

    completed tile requirements

--- frontend/src/components/PartnerTile.jsx
+++ frontend/src/components/PartnerTile.jsx
@@ -7,7 +7,7 @@ import React from 'react';
 
 function PartnerTile({ partnerData, onDelete }) {
   const activeStyle = {
-    color: partnerData.active ? '#22E00E' : 'red'
+    color: partnerData.active ? '#05B34E' : '#E40D33'
   }
 
   return (
@@ -17,7 +17,9 @@ function PartnerTile({ partnerData, onDelete }) {
       <div className="partner-info">
         <h2>{partnerData.name}</h2>
         <h3 style={activeStyle}>{partnerData.active ? 'Active' : 'Inactive'}</h3>
+        <br />
         <p>{partnerData.description}</p>
+        <br />
         <button onClick={() => onDelete(partnerData.id)}>Delete</button>
       </div>
     </div>
